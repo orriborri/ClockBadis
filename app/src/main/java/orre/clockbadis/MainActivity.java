bc85f00691467203af9ba4ad5af7262d7265a6bb
Oscar Henriksson <oscar.henriksson91@gmail.com>
2017-11-13 19:31:24 +0200

    changes the call to be assync whit future

--- app/src/main/java/orre/clockbadis/MainActivity.java
+++ app/src/main/java/orre/clockbadis/MainActivity.java
@@ -8,21 +8,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketException;
+import com.neovisionaries.ws.client.WebSocketFactory;
 
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
-
-import tech.gusavila92.websocketclient.WebSocketClient;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 
 public class MainActivity extends Activity {
-    private WebSocketClient webSocketClient;
     private TextView mTextView;
     private int myScore = 0;
     private boolean match = false;
+    ExecutorService es = Executors.newSingleThreadExecutor();
+    WebSocketFactory factory = new WebSocketFactory().setConnectionTimeout(5000);
+    private WebSocket ws = factory.createSocket("ws://klockbadis.herokuapp.com")
+            .addListener(new WebSocketAdapter() {
+                public void onTextMessage(WebSocket websocket, String message) {
+                    System.out.println(message);
+                }
+            });
+
+
+
+
+
+    Future<WebSocket> future = ws.connect(es);
+
+    public MainActivity() throws IOException {
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,65 +73,15 @@ public class MainActivity extends Activity {
     public boolean isMatch() {
         return match;
     }
-
-    public void createNewMacth(View view){
-        createWebSocketClient();
-        setContentView(R.layout.round_activity_main);
-        mTextView = findViewById(R.id.text);
-
+    public void createNewMacth(View view) throws Exception {
+        future.get();
+        ws.getSocket();
     }
-    private void createWebSocketClient() {
-        URI uri;
-        try {
-            uri = new URI("wss://klockbadis.herokuapp.com");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return;
-        }
 
-        webSocketClient = new WebSocketClient(uri) {
-            @Override
-            public void onOpen() {
-                System.out.println("onOpen");
-                webSocketClient.send("Hello, World!");
-            }
 
-            @Override
-            public void onTextReceived(String message) {
-                System.out.println("onTextReceived");
-            }
 
-            @Override
-            public void onBinaryReceived(byte[] data) {
-                System.out.println("onBinaryReceived");
-            }
 
-            @Override
-            public void onPingReceived(byte[] data) {
-                System.out.println("onPingReceived");
-            }
-
-            @Override
-            public void onPongReceived(byte[] data) {
-                System.out.println("onPongReceived");
-            }
-
-            @Override
-            public void onException(Exception e) {
-                System.out.println(e.getMessage());
-            }
-
-            @Override
-            public void onCloseReceived() {
-                System.out.println("onCloseReceived");
-            }
-        };
-        webSocketClient.setConnectTimeout(10000);
-        webSocketClient.setReadTimeout(60000);
-        webSocketClient.enableAutomaticReconnection(5000);
-        webSocketClient.connect();
-
-    }
 };
+
 
 
