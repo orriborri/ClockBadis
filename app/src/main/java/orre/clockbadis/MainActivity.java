636b5ceab616edb1fb9db6e13aa6c97b72b4ee21
Oscar Henriksson <oscar.henriksson91@gmail.com>
2017-11-13 16:15:50 +0200

    package orre.clockbadis;
    import android.app.Activity;
    import android.os.Bundle;
    import android.support.wearable.view.WatchViewStub;
    import android.view.View;
    import android.widget.Button;
    import android.widget.TextView;
    
    public class MainActivity extends Activity {
    
        private TextView mTextView;
        private int myScore = 0;
    
        @Override
        protected void onCreate(Bundle savedInstanceState) {

--- app/src/main/java/orre/clockbadis/MainActivity.java
+++ app/src/main/java/orre/clockbadis/MainActivity.java
@@ -3,43 +3,113 @@ package orre.clockbadis;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
 
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+import tech.gusavila92.websocketclient.WebSocketClient;
+
+
+
+public class MainActivity extends Activity {
+    private WebSocketClient webSocketClient;
     private TextView mTextView;
     private int myScore = 0;
+    private boolean match = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.round_activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-                mTextView.append(Integer.toString(myScore));
-            }
-        });
-        final Button buttonMinus = findViewById(R.id.buttonminus);
-        buttonMinus.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                myScore -=1;
-                mTextView.append(Integer.toString(myScore));
-            }
-        });
-        final Button buttonPlus = findViewById(R.id.buttonplus);
-        buttonPlus.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                myScore +=1;
-                mTextView.append(Integer.toString(myScore));
-            }
-        });
+        if (isMatch()) {
+            setContentView(R.layout.round_activity_main);
+            mTextView = findViewById(R.id.text);
+
+        } else {
+            setContentView(R.layout.init);
+        }
     }
 
-}
+    public void incScore(View view) {
+        myScore += 1;
+        mTextView.setText("" + myScore);
+
+    }
+
+    public void decScore(View view) {
+        myScore -= 1;
+        mTextView.setText("" + myScore);
+
+    }
+
+    public boolean isMatch() {
+        return match;
+    }
+
+    public void createNewMacth(View view){
+        createWebSocketClient();
+        setContentView(R.layout.round_activity_main);
+        mTextView = findViewById(R.id.text);
+
+    }
+    private void createWebSocketClient() {
+        URI uri;
+        try {
+            uri = new URI("wss://klockbadis.herokuapp.com");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                System.out.println("onOpen");
+                webSocketClient.send("Hello, World!");
+            }
+
+            @Override
+            public void onTextReceived(String message) {
+                System.out.println("onTextReceived");
+            }
+
+            @Override
+            public void onBinaryReceived(byte[] data) {
+                System.out.println("onBinaryReceived");
+            }
+
+            @Override
+            public void onPingReceived(byte[] data) {
+                System.out.println("onPingReceived");
+            }
+
+            @Override
+            public void onPongReceived(byte[] data) {
+                System.out.println("onPongReceived");
+            }
+
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onCloseReceived() {
+                System.out.println("onCloseReceived");
+            }
+        };
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.enableAutomaticReconnection(5000);
+        webSocketClient.connect();
+
+    }
+};
+
+
